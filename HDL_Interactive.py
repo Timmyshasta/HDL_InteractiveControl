e02c64023d3c8c49fffef8578e4b4279db311114
khuong nguyen <khuongnguyensac@gmail.com>
2019-05-29 16:44:04 -0700

    First Commit

---
+++ HDL_Interactive.py
@@ -0,0 +1,32 @@
+#Headlamp used with angle sensor for interactive adaptive headlights
+#EE Systems Team
+#Tim Medina
+
+import can
+import can.interfaces
+from can.interfaces import Bus
+from can import Message
+import subprocess
+from subprocess import call
+import time 
+import datetime
+import threading
+import keyboard
+import RPi.GPIO as GPIO
+
+bus = None
+hld = None
+Led_Blink = False
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+class HLD_Control(): # Control class for the headlamps
+    def __init__(self):
+
+        self.HLM_COMMAND_ID = 0x400
+
+        #Defined initial state of the signals
+
+        
+                 
